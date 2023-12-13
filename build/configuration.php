327ae7e33bec2a110d4239bbb5b43d02250474e4
prototypecaz <heycasimir82@hotmail.fr>
2023-12-13 12:49:42 +0100

    Initial commit

---
+++ build/configuration.php
@@ -0,0 +1,17 @@
+<?php
+
+$general = (array) array(
+    'domain' => "www.gc.com"
+);
+
+
+$mailer = (array) array(
+
+    'host' => "smtp.gmail.com",
+    'port' => "587",
+    'username' => "",
+    'password' => ""
+
+);
+
+?>
