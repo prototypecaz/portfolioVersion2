ce9a54dd7fc699d2bc46491c4384ac05e165c04a
prototypecaz <heycasimir82@hotmail.fr>
2023-12-13 12:49:42 +0100

    Initial commit

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import "./App.css";
-
-import HomePage from "./Components/HomePage";
-
-function App() {
-  return <HomePage />;
-}
-
-export default App;
--- src/Components/Techno.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Techno({ mots }) {
-  return <div className="techno">{mots}</div>;
-}
-
-export default Techno;
